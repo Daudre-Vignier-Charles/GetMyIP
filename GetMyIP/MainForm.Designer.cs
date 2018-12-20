d1a882a65fda50450e58baf8c0a48beba0d31707
Charles DAUDRE-VIGNIER <cdaudre-vignier@dsogroup.com>
2018-12-20 15:42:04 +0100

    First commit

---
+++ GetMyIP/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GetMyIP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.comboBoxLanInternet = new System.Windows.Forms.ComboBox();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxLanInternet
+            // 
+            this.comboBoxLanInternet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.comboBoxLanInternet, "comboBoxLanInternet");
+            this.comboBoxLanInternet.FormattingEnabled = true;
+            this.comboBoxLanInternet.Name = "comboBoxLanInternet";
+            this.comboBoxLanInternet.SelectedIndexChanged += new System.EventHandler(this.comboBoxLanInternet_SelectedIndexChanged);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.textBoxIP, "textBoxIP");
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.comboBoxLanInternet);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxLanInternet;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GetMyIP/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetMyIP
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LanSetting_Load();
+            comboBoxLanInternet.SelectedIndex = 0;
+            Select();
+            this.MaximumSize = this.Size;
+        }
+
+        private void LanSetting_Load()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if ((nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet) || (nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)) //&& (nic.OperationalStatus == OperationalStatus.Up))
+                {
+                    foreach (UnicastIPAddressInformation ip in nic.GetIPProperties().UnicastAddresses)
+                    {
+                        if (!ip.Address.ToString().Contains("169.254"))
+                        {
+                            comboBoxLanInternet.Items.Add(nic.Description);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void comboBoxLanInternet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation ip in nic.GetIPProperties().UnicastAddresses)
+                {
+                    if (nic.Description == comboBoxLanInternet.SelectedItem.ToString())
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            textBoxIP.ResetText();
+                            textBoxIP.Text = ip.Address.ToString();
+                            this.ActiveControl = null;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_NCHITTEST) 
+            {
+                m.Result = (IntPtr)(HT_CAPTION); // Enable drag and move MainForm
+                return;
+            }
+            else if (m.Msg == WM_NCLBUTTONDBLCLK) // Disable Double click
+            {
+                m.Result = IntPtr.Zero;
+                return;
+            }
+            base.WndProc(ref m);
+        }
+
+        // Enable drag and move MainForm
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+        // Disable Double click
+        private const int WM_NCLBUTTONDBLCLK = 0x00A3;
+    }
+}
